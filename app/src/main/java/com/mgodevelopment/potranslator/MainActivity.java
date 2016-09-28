5337b56e304eeab8ded39cd64ab502086651f749
Martin Ouellet <moa21@sfu.ca>
2016-09-27 19:42:20 -0700

    258

--- app/src/main/java/com/mgodevelopment/potranslator/MainActivity.java
+++ app/src/main/java/com/mgodevelopment/potranslator/MainActivity.java
@@ -1,18 +1,24 @@
 package com.mgodevelopment.potranslator;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.microsoft.cognitiveservices.speechrecognition.ISpeechRecognitionServerEvents;
 import com.microsoft.cognitiveservices.speechrecognition.MicrophoneRecognitionClient;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionResult;
 import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionMode;
+
+import static com.mgodevelopment.potranslator.R.id.fab;
 
 public class MainActivity extends AppCompatActivity
 implements ISpeechRecognitionServerEvents {
@@ -40,35 +46,181 @@ implements ISpeechRecognitionServerEvents {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
 
-        return super.onOptionsItemSelected(item);
+        mResultText = (TextView) findViewById(R.id.resultText);
+        onlineIcon = getResources().getIdentifier("@android:drawable/presence_audio_online", null, null);
+        busyIcon = getResources().getIdentifier("@android:drawable/ic_voice_search", null, null);
+
+        FloatingActionButton mFab = (FloatingActionButton) findViewById(fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (hasInternetConnection()) {
+                    mResultText.setText("");
+                    initRecording();
+                    if (mMicClient != null) {
+                        if (mSpeechMode.equals(SpeechRecognitionMode.ShortPhrase)) {
+                            if (!mHasStartedRecording) {
+                                mMicClient.startMicAndRecognition();
+                            }
+                        } else {
+                            if (!mHasStartedRecording) {
+                                mMicClient.startMicAndRecognition();
+                            } else {
+                                mMicClient.endMicAndRecognition();
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Please check your Internet connection", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        initLanguageSpinner();
+        initSpeechModeSpinner();
+
     }
+
+    private boolean hasInternetConnection() {
+
+        ConnectivityManager connectivityManager = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager != null
+                && connectivityManager.getActiveNetworkInfo() != null
+                && connectivityManager.getActiveNetworkInfo().isConnected();
+
+    }
+
+    private void initRecording() {
+
+    }
+
+    private void initLanguageSpinner() {
+
+        final Spinner spinner = (Spinner) findViewById(R.id.language_spinner);
+        spinner.setSaveEnabled(true);
+
+        // TODO spinner.setSelection(SharedPreferencesUtils.getBaseLanguageIndex(this));
+        // TODO mLanguageCode = Constants.LANGUAGE_CODES[SharedPreferenceUtils.getBaseLanguageIndex(this));
+
+        spinner.post(new Runnable() {
+            @Override
+            public void run() {
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                        Log.d(LOG, "in Language Spinner onItemSelected");
+                        mLanguageCode = Constants.LANGUAGE_CODES[position];
+                        mHasOptionChanged = true;
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // no action required, but we must implement this method
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void initSpeechModeSpinner() {
+
+        final Spinner spinner = (Spinner) findViewById(R.id.speech_mode_spinner);
+        spinner.setSaveEnabled(true);
+
+        // TODO int pref = SharedPreferencesUtils.getSpeechModeIndex(this);
+        // TODO spinner.setSelection(pref);
+        // TODO mSpeechMode = pref == 0 ? SpeechRecognitionMode.ShortPhrase : SpeechRecognitionMode.LongDictation
+
+        spinner.post(new Runnable() {
+            @Override
+            public void run() {
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                        Log.d(LOG, "in Speech Mode Spinner onItemSelected");
+                        mSpeechMode = position == 0 ? SpeechRecognitionMode.ShortPhrase : SpeechRecognitionMode.LongDictation;
+                        mHasOptionChanged = true;
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // no action required, but we must implement this method
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    public void onPartialResponseReceived(String s) {
+
+    }
+
+    @Override
+    public void onIntentReceived(String s) {
+
+    }
+
+    @Override
+    public void onError(int i, String s) {
+
+    }
+
+    @Override
+    public void onAudioEvent(boolean b) {
+
+    }
+
+    @Override
+    public void onFinalResponseReceived(RecognitionResult recognitionResult) {
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
