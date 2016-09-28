3a74a0c3d9b76df60059f004c5ceafefb157d614
Martin Ouellet <moa21@sfu.ca>
2016-09-27 21:12:05 -0700

    259

--- app/src/main/java/com/mgodevelopment/potranslator/MainActivity.java
+++ app/src/main/java/com/mgodevelopment/potranslator/MainActivity.java
@@ -16,14 +16,16 @@ import android.widget.Toast;
 import com.microsoft.cognitiveservices.speechrecognition.ISpeechRecognitionServerEvents;
 import com.microsoft.cognitiveservices.speechrecognition.MicrophoneRecognitionClient;
 import com.microsoft.cognitiveservices.speechrecognition.RecognitionResult;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionStatus;
 import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionMode;
+import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionServiceFactory;
 
 import static com.mgodevelopment.potranslator.R.id.fab;
 
 public class MainActivity extends AppCompatActivity
-implements ISpeechRecognitionServerEvents {
+        implements ISpeechRecognitionServerEvents {
 
-    public static final String LOG = "POTranslator";
+    public static final String TAG = "POTranslator";
     private MicrophoneRecognitionClient mMicClient = null;
     private SpeechRecognitionMode mSpeechMode = SpeechRecognitionMode.ShortPhrase;
 
@@ -97,6 +99,21 @@ implements ISpeechRecognitionServerEvents {
 
     private void initRecording() {
 
+        if (mHasOptionChanged || mMicClient == null) {
+
+            Log.d(TAG, "Language is " + mLanguageCode + "\nSpeech mode is " + mSpeechMode);
+
+            if (mKey.equals(Constants.PRIMARY_SUBSCRIPTION_KEY)) {
+                mResultText.append("Connecting with PRIMARY KEY \n");
+            } else {
+                mResultText.append("Connecting with SECONDARY KEY \n");
+            }
+
+            mMicClient = SpeechRecognitionServiceFactory.createMicrophoneClient(this, mSpeechMode, mLanguageCode, this, mKey);
+            mHasOptionChanged = false;
+
+        }
+
     }
 
     private void initLanguageSpinner() {
@@ -114,7 +131,7 @@ implements ISpeechRecognitionServerEvents {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                        Log.d(LOG, "in Language Spinner onItemSelected");
+                        Log.d(TAG, "in Language Spinner onItemSelected");
                         mLanguageCode = Constants.LANGUAGE_CODES[position];
                         mHasOptionChanged = true;
 
@@ -146,7 +163,7 @@ implements ISpeechRecognitionServerEvents {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                        Log.d(LOG, "in Speech Mode Spinner onItemSelected");
+                        Log.d(TAG, "in Speech Mode Spinner onItemSelected");
                         mSpeechMode = position == 0 ? SpeechRecognitionMode.ShortPhrase : SpeechRecognitionMode.LongDictation;
                         mHasOptionChanged = true;
 
@@ -163,31 +180,90 @@ implements ISpeechRecognitionServerEvents {
     }
 
     @Override
-    public void onPartialResponseReceived(String s) {
-
-    }
-
-    @Override
-    public void onIntentReceived(String s) {
-
-    }
-
-    @Override
-    public void onError(int i, String s) {
-
-    }
-
-    @Override
-    public void onAudioEvent(boolean b) {
-
+    public void onPartialResponseReceived(String response) {
+        mResultText.append("PARTIAL RESULT:\n");
+        mResultText.append(response + "\n");
     }
 
     @Override
     public void onFinalResponseReceived(RecognitionResult recognitionResult) {
 
+        // explanation of results at https://msdn.microsoft.com/en-us/library/mt613453.aspx
+        boolean isFinalDictationMessage = (
+                mSpeechMode == SpeechRecognitionMode.LongDictation
+                        && (
+                        recognitionResult.RecognitionStatus == RecognitionStatus.EndOfDictation
+                                || recognitionResult.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout
+                                || recognitionResult.RecognitionStatus == RecognitionStatus.RecognitionSuccess
+                ));
+        if (mSpeechMode == SpeechRecognitionMode.ShortPhrase || isFinalDictationMessage) {
+
+            if (mMicClient != null) {
+                mMicClient.endMicAndRecognition();
+            }
+
+            mFab.setEnabled(true);
+            mFab.setImageResource(onlineIcon);
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (isFinalDictationMessage) {
+            sb.append("Final Dictation Message");
+        }
+
+        if (recognitionResult.Results.length > 0) {
+            sb.append("Text Suggestions\n");
+            for (int i = 0; i < recognitionResult.Results.length; i++) {
+                sb.append((i + 1) + " " + recognitionResult.Results[i].DisplayText + "\n");
+            }
+            sb.append("\n" + mResultText.getText().toString());
+            mResultText.setText(sb.toString());
+        }
+
     }
 
-//    @Override
+    @Override
+    public void onIntentReceived(String response) {
+        // We're not using speech recognition with intent but
+        // we must implement all the interface methods.
+    }
+
+    @Override
+    public void onError(int errorCode, String response) {
+
+        mFab.setEnabled(true);
+        mFab.setImageResource(onlineIcon);
+        Toast.makeText(this, "Cannot connect to server!\nError has occured!", Toast.LENGTH_SHORT).show();
+        mResultText.append("Error " + errorCode + ": " + response + "\n");
+        mMicClient = null; // Force an initialization when recording next time
+        mKey = mKey == Constants.PRIMARY_SUBSCRIPTION_KEY ? Constants.SECONDARY_SUBSCRIPTION_KEY : Constants.PRIMARY_SUBSCRIPTION_KEY;
+
+    }
+
+    @Override
+    public void onAudioEvent(boolean isRecording) {
+
+        mHasStartedRecording = isRecording;
+        if (!isRecording) {
+
+            if (mMicClient != null) {
+                mMicClient.endMicAndRecognition();
+            }
+            mFab.setEnabled(true);
+            mFab.setImageResource(onlineIcon);
+
+        } else {
+            if (mSpeechMode == SpeechRecognitionMode.ShortPhrase) {
+                mFab.setEnabled(false);
+            }
+            mFab.setImageResource(busyIcon);
+        }
+        mResultText.append(isRecording ? "RECORDING STARTED\n" : "RECORDING ENDED\n");
+
+    }
+
+    //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -208,19 +284,6 @@ implements ISpeechRecognitionServerEvents {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
